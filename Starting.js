3b963ac7f435c02841654d3ae37d74875868b8e0
Vivek Singh Mehta <viveksinghmehta.smr@gmail.com>
2019-12-08 12:20:10 +0530

    Added package.json

---
+++ Starting.js
@@ -0,0 +1,13 @@
+/*
+ * Filename: /Users/apple/Documents/Vivek/ShopPOS/Starting.js
+ * Path: /Users/apple/Documents/Vivek/ShopPOS
+ * Created Date: Thursday, December 5th 2019, 10:49:19 pm
+ * Author: Vivek Singh Mehta
+ * 
+ * Copyright (c) 2019 Vivek Singh Mehta
+ */
+
+// function hello() {
+//     console.log("Hello world");
+// }
+alert("Hello User");
