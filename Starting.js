d389e3562694ba5c45f464b1f7b295294f840b12
Vivek Singh Mehta <viveksinghmehta.smr@gmail.com>
2019-12-08 13:08:56 +0530

    created server

--- Starting.js
+++ Starting.js
@@ -6,8 +6,24 @@
  * 
  * Copyright (c) 2019 Vivek Singh Mehta
  */
+import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
 
-// function hello() {
-//     console.log("Hello world");
-// }
-alert("Hello User");
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+
+app.get("/test", function (request, response) {
+    response.end("TEsting");
+})
+
+const app = express()
+
+const httpServer = http.createServer(app)
+
+httpServer.listen(process.env.PORT || 1526)
