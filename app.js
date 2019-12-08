1341f36da2c15857fe597720014f023a3aa786fd
Vivek Singh Mehta <viveksinghmehta.smr@gmail.com>
2019-12-08 14:03:20 +0530

    added a new file

---
+++ app.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
+app.use(jsonParser);
+app.use(urlencodedParser);
+
+app.get("/test", function (request, response) {
+    let newJson = {
+        "id": "0001",
+        "type": "donut",
+        "name": "Cake",
+        "image":
+        {
+            "url": "images/0001.jpg",
+            "width": 200,
+            "height": 200
+        },
+        "thumbnail":
+        {
+            "url": "images/thumbnails/0001.jpg",
+            "width": 32,
+            "height": 32
+        }
+    }
+    response.status(200);
+    response.json(newJson);
+});
+
+const httpServer = http.createServer(app);
+
+httpServer.listen(process.env.PORT || 1526);
+console.log(process.env.PORT)
+console.log('server is up and running');
